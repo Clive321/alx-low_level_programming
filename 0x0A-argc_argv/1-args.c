086e15cd44093de624e3c244b4f797c3e8acd54d
Clive321 <lavitaclive@gmail.com>
2023-03-10 21:20:14 +0000

    whtasmyname

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program printing name of itself
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always: 0 (Success)
+ */
+
+int main(int argc_atribue_((unused)), char *argv[])
+
+{
+	printf("%s\n", *argv);
+	return (0);
+}
